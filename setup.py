f43d2f1e50959d7405ed55223635d4f9a494fb2e
Philipp Hanemann <philipp.hanemann@posteo.de>
2017-01-13 17:52:01 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='phuncs',
+      version='0.1',
+      description='phils funcs',
+      long_description='Just a helper package with useful functions',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+      ],
+      keywords='functions useful',
+      author='Philipp Hanemann',
+      author_email='philipp.hanemann@posteo.de',
+      license='MIT',
+      packages=['phuncs'],
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False)
